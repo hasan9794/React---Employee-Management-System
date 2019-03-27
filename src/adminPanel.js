1cff3a7f4df38ad51b4262699eb2f0f0b25b46cf
hasan9794 <hasanm9794@gmail.com>
2019-03-27 12:15:26 +0500

    Added new Feature - Addition/Deletion

--- src/adminPanel.js
+++ src/adminPanel.js
@@ -1,6 +1,57 @@
 import React from "react"
+import "./adminPanel.css"
+
 
 class Admin extends React.Component {
+    constructor(){
+        super()
+
+        this.state ={
+            inputArray: [],
+            addBtn: false
+        }
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+        console.log([event.target.value])
+    }
+
+    handleClick(){
+        let obj = {
+            fName: this._firstName.value ,
+            lName: this._lastName.value ,
+            email: this._email.value ,
+            salary: this._salary.value ,
+            date: this._date.value,
+            id: Date.now()
+        }
+        console.log(obj)
+        
+        let arr = this.state.inputArray;
+        arr.push(obj)
+        console.log(arr)
+        
+        this.setState({
+            inputArray: arr,
+            addBtn: !this.state.addBtn
+        })
+    }
+
+    del(id){
+        let element = this.state.inputArray.filter((e) =>{
+            if(e.id !== id){
+                return true
+            }
+        })
+
+        this.setState({
+            inputArray: element
+        })
+    }
+
     render(){
         
         return(
@@ -14,7 +65,7 @@ class Admin extends React.Component {
                 </div>
             </nav>
                 <div className="container">
-                    <div class="row">
+                { !this.state.addBtn && <div class="row">
                         <table style={{marginTop: 30}} className="striped">
                             <tr>
                                 <th>First Name</th>
@@ -23,24 +74,70 @@ class Admin extends React.Component {
                                 <th>Salary</th>
                                 <th>Job Start Date</th>
                             </tr>
-                            <tr>
-                                <td>Muhammad</td>
-                                <td>Hasan</td>
-                                <td>hasan@gmail.com</td>
-                                <td>20,00000</td>
-                                <td>1, January 2020</td>
-                            </tr>
-                            <tr>
-                                <td>Muhammad</td>
-                                <td>Hasan</td>
-                                <td>hasan@gmail.com</td>
-                                <td>20,00000</td>
-                                <td>1, January 2020</td>
-                            </tr>
+                            
+                                {   
+                                    
+                                    this.state.inputArray.map((e) => 
+                                            <tr>
+                                                <td>{e.fName}</td>
+                                                <td>{e.lName}</td>
+                                                <td>{e.email}</td>
+                                                <td>{e.salary}</td>
+                                                <td>{e.date}</td>
+                                                <td><i onClick={this.del.bind(this,e.id)} class="far fa-trash-alt ui"></i></td>
+                                            </tr>
+                                        
+                                            
+                                    )
+                                
+                                }  
+                            
                         </table>
+                    </div>}
+                    {this.state.addBtn && <div>
+
+                    <div>
+                        <h2>Add an Employee</h2>
                     </div>
+                    <div className="row">
+                        <div class="input-field col s6">
+                            <input ref={(s) => this._firstName = s} name="fName" id="first_name" type="text" class="validate"  />
+                            <label for="first_name">First Name</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input ref={(s) => this._lastName = s} name="lName" id="last_name" type="text" class="validate"  />
+                            <label for="last_name">Last Name</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input ref={(s) => this._email = s} name="email" id="email" type="email" class="validate"  />
+                            <label for="email">Email</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input ref={(s) => this._salary = s} name="salary" id="salary" type="number" class="validate"  />
+                            <label for="salary">Salary</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input ref={(s) => this._date = s} name="date" id="date" type="text" class="datepicker"  />
+                            <label for="date">Date of Joining (eg. 04/12/2017)</label>
+                        </div>
+                        <div className="input-field">
+                            <div style={{width: "100%"}} className="col">
+                                <button onClick={this.handleClick.bind(this)} style={{width: "100%", marginTop: 20}} class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div> }
                 </div>
+                <a onClick={() => {this.setState({addBtn: !this.state.addBtn})}} href class="float">
+                    <i className="fa fa-plus my-float"></i>
+                </a>
+                    
             </div>
+            
         )
     }
 }
